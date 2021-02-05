7bad1facc14b8b39e8fdf9afee5ea1c0181bee40
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-05 09:22:15 +0100

    Limit to supported architecture

--- Formula/runc-bin.rb
+++ Formula/runc-bin.rb
@@ -1,11 +1,14 @@
 class RuncBin < Formula
   desc "CLI tool for spawning and running containers according to the OCI specification"
   homepage "https://www.opencontainers.org/"
-
-  version "1.0.0-rc92"
-  url "https://github.com/opencontainers/runc/releases/download/v#{version}/runc.amd64"
-  sha256 "256bd490a55a1939a4c9cd15c043404b79a86429ee04129c00d33dab8c0cf040"
   license "Apache-2.0"
+
+  if Hardware::CPU.intel?
+    url "https://github.com/opencontainers/runc/releases/download/v1.0.0-rc92/runc.amd64"
+    sha256 "256bd490a55a1939a4c9cd15c043404b79a86429ee04129c00d33dab8c0cf040"
+  else
+    odie "Only amd64 is supported"
+  end
 
   bottle :unneeded
 
