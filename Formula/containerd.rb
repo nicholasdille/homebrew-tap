04e8c8544640d32a68296b1f79d1bd54a32cddff
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-05 11:11:09 +0100

    Added containerd

---
+++ Formula/containerd.rb
@@ -0,0 +1,25 @@
+class Containerd < Formula
+  desc "Open and reliable container runtime"
+  homepage "https://containerd.io/"
+  url "https://github.com/containerd/containerd.git",
+    tag: "v1.4.3"
+  license "Apache-2.0"
+  head "https://github.com/containerd/containerd.git"
+
+  depends_on "go" => :build
+  depends_on "go-md2man" => :build
+  depends_on "libseccomp" => :build
+  depends_on "pkg-config" => :build
+
+  conflicts_with "nicholasdille/tap/containerd-bin"
+  conflicts_with "nicholasdille/tap/docker-bin"
+
+  def install
+    system "make"
+    system "make", "install", "DESTDIR=#{prefix}"
+  end
+
+  test do
+    system "#{bin}/containerd", "--version"
+  end
+end
