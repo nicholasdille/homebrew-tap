9a82e21f73603acd59e3f11022b1d14fe933cae9
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-05 08:37:48 +0100

    Fixes after style check

--- Formula/runc-bin.rb
+++ Formula/runc-bin.rb
@@ -1,15 +1,15 @@
 class RuncBin < Formula
   desc "CLI tool for spawning and running containers according to the OCI specification"
   homepage "https://www.opencontainers.org/"
-  
-  url "https://github.com/opencontainers/runc/releases/download/#{version}/runc.amd64"
-  version "v1.0.0-rc92"
+
+  url "https://github.com/opencontainers/runc/releases/download/v#{version}/runc.amd64"
+  version "1.0.0-rc92"
   sha256 "256bd490a55a1939a4c9cd15c043404b79a86429ee04129c00d33dab8c0cf040"
   license "Apache-2.0"
 
   bottle :unneeded
 
-  conflicts_with "nicholasdille/tap/runc", :because => "both install `runc` binary"
+  conflicts_with "nicholasdille/tap/runc", because: "both install `runc` binary"
 
   def install
     bin.install "runc.amd64" => "runc"
