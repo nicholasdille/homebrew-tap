6c98e03603d70685d16b84fbd4ed04317ea5a59f
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-05 10:35:02 +0100

    Added formula to build from source

---
+++ Formula/slirp4netns.rb
@@ -0,0 +1,30 @@
+class Slirp4netns < Formula
+  desc "User-mode networking for unprivileged network namespaces"
+  homepage "https://github.com/rootless-containers/slirp4netns"
+  url "https://github.com/rootless-containers/slirp4netns.git",
+    tag: "v1.1.8"
+  license "GPL-2.0-or-later"
+  head "https://github.com/rootless-containers/slirp4netns.git"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "gcc" => :build
+  depends_on "libcap" => :build
+  depends_on "libseccomp" => :build
+  depends_on "libslirp" => :build
+  depends_on "glib" => :build
+  depends_on "pkg-config" => :build
+
+  conflicts_with "nicholasdille/tap/slirp4netns-bin"
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/slirp4netns", "--version"
+  end
+end
