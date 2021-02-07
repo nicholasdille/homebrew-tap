977dd3ef0e3c02c7ff94a0b5ac92931c75855d64
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-07 14:09:54 +0100

    Added script to test formula and create bottle

---
+++ scripts/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -o errexit
+
+FORMULA=$1
+if test -z "${FORMULA}"; then
+    echo "ERROR: Formula must be specified."
+    exit 1
+fi
+
+function brew() {
+    docker run \
+        --interactive \
+        --rm \
+        --user "$(id -u):$(id -g)" \
+        --mount type=bind,src=/home/linuxbrew,dst=/home/linuxbrew \
+        homebrew/brew \
+        brew "$@"
+}
+
+brew style "${FORMULA}"
+brew audit "${FORMULA}"
+brew install --build-bottle "${FORMULA}"
+brew bottle "${FORMULA}"
