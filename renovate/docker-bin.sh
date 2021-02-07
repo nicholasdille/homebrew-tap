9e44d7dd4381fa9f8acfbea7b42926855e96f883
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-07 13:52:09 +0100

    Update url in formula

--- renovate/docker-bin.sh
+++ renovate/docker-bin.sh
@@ -4,4 +4,12 @@ DATASOURCE=github-tags
 REPO=docker/cli
 VERSION=20.10.2
 
-#
+# Update version
+for FORMULA in docker-bin docker-rootless-bin; do
+    if test -f "Formula/${FORMULA}.rb"; then
+        sed -E -i "s|/docker-(.+).tgz|/docker-20.10.2.tgz|" "Formula/${FORMULA}.rb"
+    fi
+done
+
+# Update SHA256
+#TODO
