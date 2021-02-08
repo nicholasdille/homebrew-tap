5c5babd6a90b69280c76eb4bf0e97b1d52d4ebf5
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-08 16:04:43 +0100

    Downgrade for testing

--- Formula/docker-bin.rb
+++ Formula/docker-bin.rb
@@ -2,18 +2,18 @@ class DockerBin < Formula
   desc "Docker CLI"
   homepage "https://www.docker.com"
   if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-    url "https://download.docker.com/linux/static/stable/x86_64/docker-20.10.3.tgz"
+    url "https://download.docker.com/linux/static/stable/x86_64/docker-20.10.2.tgz"
     sha256 "47065a47f0692cd5af03073c7386fe090d9ef5ac88a7d8455a884d8e15809be5"
   elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://download.docker.com/linux/static/stable/aarch64/docker-20.10.3.tgz"
+    url "https://download.docker.com/linux/static/stable/aarch64/docker-20.10.2.tgz"
     sha256 "4dcd105f721297f314bb53622e67dd981a743d72f4b2bfe4f42a8790e0892c82"
   elsif Hardware::CPU.ppc64le?
-    url "https://download.docker.com/linux/static/stable/ppc64le/docker-20.10.3.tgz"
+    url "https://download.docker.com/linux/static/stable/ppc64le/docker-20.10.2.tgz"
     sha256 "f88dd3635386d6e658ca970f8442fff97981e2239d2cf88b62e94d4302539f52"
   else
     odie "Processor architecture is not supported."
   end
-  version "20.10.3"
+  version "20.10.2"
   license "Apache-2.0"
 
   bottle :unneeded
