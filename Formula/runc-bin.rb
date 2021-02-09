c24c7df34b230b7add7b47f282e57b69aee1c68d
nicholasdille <nicholasdille@users.noreply.github.com>
2021-02-09 13:22:44 +0000

    Updated formulae

--- Formula/runc-bin.rb
+++ Formula/runc-bin.rb
@@ -2,12 +2,12 @@ class RuncBin < Formula
   desc "CLI tool for spawning and running containers according to the OCI specification"
   homepage "https://www.opencontainers.org/"
   if Hardware::CPU.intel?
-    url "https://github.com/opencontainers/runc/releases/download/v1.0.0-rc93/runc.amd64"
-    sha256 "9feaa82be15cb190cf0ed76fcb6d22841abd18088d275a47e894cd1e3a0ee4b6"
+    url "https://github.com/opencontainers/runc/releases/download/v1.0.0-rc91/runc.amd64"
+    sha256 "c1c30e250ba1af1bc513a37a85aef8c7824e9b8e76d10c933add58a72d84fce6"
   else
     odie "Only amd64 is supported"
   end
-  version "1.0.0-rc93"
+  version "1.0.0-rc91"
   license "Apache-2.0"
 
   bottle :unneeded
