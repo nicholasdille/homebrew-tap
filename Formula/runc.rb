d98e905bbb3c87c496b249de7de01970bc6a34f4
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-11 19:54:58 +0100

    Bottle for runc
    Closes #99.
    
    Signed-off-by: Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>

--- Formula/runc.rb
+++ Formula/runc.rb
@@ -1,6 +1,6 @@
 class Runc < Formula
   desc "CLI tool for spawning and running containers according to the OCI specification"
-  homepage "https://www.opencontainers.org/"
+  homepage "https://www.opencontainers.org"
 
   url "https://github.com/opencontainers/runc.git",
     tag:      "v1.0.0-rc93",
