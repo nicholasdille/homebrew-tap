2cf4c1fe8ad4c6ba535447efdeedd73640356531
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-02-11 19:10:48 +0000

    kubeone: add 1.2.0-beta.0 bottle.

--- Formula/kubeone.rb
+++ Formula/kubeone.rb
@@ -8,6 +8,11 @@ class Kubeone < Formula
   license "Apache-2.0"
   head "https://github.com/kubermatic/kubeone.git"
 
+  bottle do
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/kubeone-1.2.0-beta.0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d72c0bf0b4d552dc0f93fa17ffa7fc25cc263c01aee38ca3a80f7d7264b16c56"
+  end
+
   depends_on "go" => :build
 
   def install
