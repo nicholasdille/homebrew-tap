a6fe3e513b2a7cd457df87edbd6143bb6c41298f
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-02-11 19:05:02 +0000

    clusterctl: add 0.3.14 bottle.

--- Formula/clusterctl.rb
+++ Formula/clusterctl.rb
@@ -8,6 +8,11 @@ class Clusterctl < Formula
   license "Apache-2.0"
   head "https://github.com/kubernetes-sigs/cluster-api.git"
 
+  bottle do
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/clusterctl-0.3.14"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "46a9ff62ce3216aeffab44270ddad7f5304c131b315665c3c92390f47ee6881b"
+  end
+
   depends_on "go" => :build
 
   def install
