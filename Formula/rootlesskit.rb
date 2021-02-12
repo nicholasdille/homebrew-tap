c15b23f155a5fa68895f0dac04abb80595ee1bb8
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-02-12 07:53:06 +0000

    rootlesskit: add 0.13.0 bottle.

--- Formula/rootlesskit.rb
+++ Formula/rootlesskit.rb
@@ -8,6 +8,11 @@ class Rootlesskit < Formula
   license "Apache-2.0"
   head "https://github.com/rootless-containers/rootlesskit.git"
 
+  bottle do
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/rootlesskit-0.13.0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "476e7a78d53fe6172f651936de73c92da0d9cae05d5f31eaea7e698ad87ee2e1"
+  end
+
   depends_on "go" => :build
 
   def install
