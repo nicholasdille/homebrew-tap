49b9d91883951c4f88d3933d12459bfa9489114b
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-12 10:40:25 +0100

    Manually added bottle

--- Formula/slirp4netns.rb
+++ Formula/slirp4netns.rb
@@ -8,6 +8,11 @@ class Slirp4netns < Formula
   license "GPL-2.0-or-later"
   head "https://github.com/rootless-containers/slirp4netns.git"
 
+  bottle do
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/slirp4netns-1.1.8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "6ee33373c0a8764e25e59247ecb8ab1a77896226bd5d696a7b811f78a57f0a6f"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "gcc" => :build
