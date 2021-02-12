ae4afb1f22f1067d15c8e5016b6cd8c8266a8ce9
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-12 08:51:14 +0100

    Simplified build

--- Formula/runc.rb
+++ Formula/runc.rb
@@ -13,45 +13,26 @@ class Runc < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "78d9d1e617a2b7767bf89853b9d831bfaa3a2b9d25b44a545f6950475c008c6f"
   end
 
+  option "with-nokmem", "Disable kernel memory accounting"
   depends_on "go" => :build
   depends_on "go-md2man" => :build
-  depends_on "libseccomp" => :build
+  depends_on "libseccomp" => [:build, :recommended]
   depends_on "pkg-config" => :build
 
   def install
-    dir = buildpath/"src/github.com/opencontainers/runc"
-    dir.install (buildpath/"").children
-    cd dir do
-      buildtags = [
-        "seccomp",
-      ]
-      commit = Utils.git_short_head
-      ENV["GOPATH"] = buildpath
-      ENV["CGO_ENABLED"] = "1"
-      ldflags = [
-        "-w",
-        "-extldflags",
-        "-static",
-        "-X",
-        "main.gitCommit=#{commit}",
-        "-X",
-        "main.version=#{version}",
-      ]
-      system "go", "build",
-        "-trimpath",
-        "-tags", "#{buildtags} netgo osusergo",
-        "-ldflags", ldflags.join(" "),
-        "-o", bin/"runc",
-        "."
+    buildtags = []
+    buildtags << "seccomp" if build.with?("libseccomp")
+    buildtags << "nokmem" if build.with?("nokmem")
+    system "make", "static", "BUILDTAGS=#{buildtags.join(" ")}"
+    bin.install "runc"
 
-      Pathname.glob("man/*.[1-8].md") do |md|
-        section = md.to_s[/\.(\d+)\.md\Z/, 1]
-        (man/"man#{section}").mkpath
-        system "go-md2man", "-in=#{md}", "-out=#{man/"man#{section}"/md.stem}"
-      end
-
-      bash_completion.install "contrib/completions/bash/runc"
+    Pathname.glob("man/*.[1-8].md") do |md|
+      section = md.to_s[/\.(\d+)\.md\Z/, 1]
+      (man/"man#{section}").mkpath
+      system "go-md2man", "-in=#{md}", "-out=#{man/"man#{section}"/md.stem}"
     end
+
+    bash_completion.install "contrib/completions/bash/runc"
   end
 
   test do
