4ff5475776884af5fb2d2f385c3123c6802b76d0
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-15 11:07:40 +0100

    Updated rootlesskit-bin to 0.13.1

--- Formula/rootlesskit-bin.rb
+++ Formula/rootlesskit-bin.rb
@@ -2,9 +2,9 @@ class RootlesskitBin < Formula
   desc "Linux-native 'fake root' for implementing rootless containers"
   homepage "https://github.com/rootless-containers/rootlesskit"
 
-  url "https://github.com/rootless-containers/rootlesskit/releases/download/v0.13.0/rootlesskit-x86_64.tar.gz"
-  version "0.13.0"
-  sha256 "6c7422729bb4c7adaed96e30e9f9145f955e1c71296cab97ee419b43ec5a31fa"
+  url "https://github.com/rootless-containers/rootlesskit/releases/download/v0.13.1/rootlesskit-x86_64.tar.gz"
+  version "0.13.1"
+  sha256 "f951d9681860926277396a288898963da1b874ce9a89ec9dea338d0eaf001c1f"
   license "Apache-2.0"
 
   bottle :unneeded
