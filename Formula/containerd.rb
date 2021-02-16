faaaf8ec07482d159787fc91c400dcf925834028
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-16 15:42:40 +0100

    Added revision for new bottle

--- Formula/containerd.rb
+++ Formula/containerd.rb
@@ -6,6 +6,7 @@ class Containerd < Formula
     tag:      "v1.4.3",
     revision: "269548fa27e0089a8b8278fc4fc781d7f65a939b"
   license "Apache-2.0"
+  revision 2
   head "https://github.com/containerd/containerd.git"
 
   depends_on "go" => :build
