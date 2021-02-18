f36ef353fd9618fc78bf41f591363cec4531782a
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-02-18 15:39:18 +0000

    cni-isolation: add 0.0.3 bottle.

--- Formula/cni-isolation.rb
+++ Formula/cni-isolation.rb
@@ -8,6 +8,11 @@ class CniIsolation < Formula
   license "Apache-2.0"
   head "https://github.com/AkihiroSuda/cni-isolation.git"
 
+  bottle do
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/cni-isolation-0.0.3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "af0d6007f6a0ecf0b14ed1dc650e30249b4c6f24b4a6c43f76babf92eece6ebc"
+  end
+
   depends_on "go" => :build
 
   def install
