36669b7a14c31d6e131888fea446a6ad645bdfa5
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-02-19 10:32:28 +0000

    kapp: add 0.35.0 bottle.

--- Formula/kapp.rb
+++ Formula/kapp.rb
@@ -7,6 +7,11 @@ class Kapp < Formula
     revision: "8e8ab96d9597e160ba6f813b1d92710c2a2d0068"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/kapp-0.35.0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b66975bf08833d9e023132728f97d0c06e2fb2174b5f7660329b96e9aab9177f"
+  end
+
   depends_on "go" => :build
   depends_on "nicholasdille/tap/ytt" => :build
   depends_on "zip" => :build
