e427605073079e904da1631f17164bc233acb055
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-24 14:38:33 +0100

    Added trivy

---
+++ Formula/trivy.rb
@@ -0,0 +1,25 @@
+class Trivy < Formula
+  desc "Simple and Comprehensive Vulnerability Scanner for Containers, Suitable for CI"
+  homepage "https://github.com/aquasecurity/trivy"
+
+  url "https://github.com/aquasecurity/trivy.git",
+    tag:      "v0.15.0",
+    revision: "08ca1b00b729b2a83c0e9f2f6e8d1a9f0110c455"
+  license "Apache-2.0"
+  head "https://github.com/aquasecurity/trivy.git"
+
+  depends_on "go" => :build
+  #depends_on "git" => :build
+
+  def install
+    tag = Utils.safe_popen_read("git", "describe", "--tags")
+    ENV["GO111MODULE"] = "on"
+    ENV["CGO_ENABLED"] = "0"
+    system "go", "build", "-ldflags", "-s -w -X=main.version=#{tag}", "./cmd/trivy"
+    bin.install "trivy"
+  end
+
+  test do
+    system "#{bin}/trivy", "--version"
+  end
+end
