918d688967c24874c2e2fee9109bf59a07ab3406
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-24 14:43:13 +0100

    Added buildx source formula

---
+++ Formula/buildx.rb
@@ -0,0 +1,32 @@
+class Buildx < Formula
+  desc "Docker CLI plugin for extended build capabilities with BuildKit"
+  homepage "https://www.docker.com"
+
+  url "https://github.com/docker/buildx.git",
+    tag:      "v0.5.1",
+    revision: "11057da37336192bfc57d81e02359ba7ba848e4a"
+  license "Apache-2.0"
+  revision 1
+  head "https://github.com/docker/buildx.git"
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "binaries"
+
+    bin.install "bin/buildx" => "docker-buildx"
+  end
+
+  def caveats
+    <<~EOS
+      You should create a symlink to enable the Docker CLI plugin:
+
+      mkdir -p $HOME/.docker/cli-plugins
+      ln -s #{lib}/docker/cli-plugins/docker-buildx $HOME/.docker/cli-plugins
+    EOS
+  end
+
+  test do
+    system "#{bin}/buildx", "--version"
+  end
+end
