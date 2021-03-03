e3e44083385da5db17df5ae7eaad4d3c1dff3a69
Nicholas Dille <nicholas.dille@mailbox.org>
2021-03-03 11:03:06 +0100

    Added podman
    Closes #150.
    
    Signed-off-by: Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>

---
+++ Formula/podman.rb
@@ -0,0 +1,28 @@
+class Podman < Formula
+  desc "Tool for managing OCI containers and pods"
+  homepage "https://podman.io/"
+
+  url "https://github.com/containers/podman.git",
+    tag:      "v3.0.1",
+    revision: "c640670e85c4aaaff92741691d6a854a90229d8d"
+  license "Apache-2.0"
+  head "https://github.com/containers/podman.git"
+
+  depends_on "go" => :build
+  depends_on "go-md2man" => :build
+
+  def install
+    system "make", "podman-remote-static"
+    bin.install "bin/podman-remote-static" => "podman"
+
+    system "make", "docs"
+    man1.install Dir["docs/build/man/*.1"]
+
+    bash_completion.install "completions/bash/podman"
+    zsh_completion.install "completions/zsh/_podman"
+  end
+
+  test do
+    system "#{bin}/podman", "--version"
+  end
+end
