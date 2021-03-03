29db08229541fbbf3fedf297beedd8292cd28f23
Nicholas Dille <nicholas.dille@mailbox.org>
2021-03-03 10:12:51 +0100

    Added footloose

---
+++ Formula/footloose.rb
@@ -0,0 +1,27 @@
+class Footloose < Formula
+  desc "Containers that look like Virtual Machines"
+  homepage "https://github.com/weaveworks/footloose"
+
+  url "https://github.com/weaveworks/footloose.git",
+    tag:      "0.6.3",
+    revision: "352435f62e04975f1aff08cfdc91241eaa163f36"
+  license "Apache-2.0"
+  head "https://github.com/weaveworks/footloose.git"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    system "go", "mod", "vendor"
+    system "go",
+      "build",
+      "-mod=vendor",
+      "-o",
+      "#{bin}/footloose",
+      "."
+  end
+
+  test do
+    system "#{bin}/footloose", "--version"
+  end
+end
