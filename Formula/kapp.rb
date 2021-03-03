46d3d01c42c55a5e49863c9b122e9ec1fca930a8
Nicholas Dille <nicholas.dille@mailbox.org>
2021-03-03 12:49:27 +0100

    Simplified commands

--- Formula/kapp.rb
+++ Formula/kapp.rb
@@ -6,20 +6,19 @@ class Kapp < Formula
     tag:      "v0.35.0",
     revision: "8e8ab96d9597e160ba6f813b1d92710c2a2d0068"
   license "Apache-2.0"
-  revision 1
+  revision 2
   head "https://github.com/vmware-tanzu/carvel-kapp.git"
-
-  bottle do
-    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/kapp-0.35.0_1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "acb222505e599ca85a15ccacd989fa686418e2fedfc07d30cef97ecdd9f75281"
-  end
 
   depends_on "go" => :build
 
   def install
     ENV["CGO_ENABLED"] = "0"
-    system "go", "build", "-ldflags=-buildid=", "-trimpath", "-o", "kapp", "./cmd/kapp"
-    bin.install "kapp"
+    system "go",
+      "build",
+      "-ldflags=-buildid=",
+      "-trimpath",
+      "-o", "#{bin}/kapp",
+      "./cmd/kapp"
   end
 
   test do
--- Formula/ytt.rb
+++ Formula/ytt.rb
@@ -6,20 +6,19 @@ class Ytt < Formula
     tag:      "v0.31.0",
     revision: "0b8c690e3dc3edeba1e819d13de67ec3c22aa322"
   license "Apache-2.0"
-  revision 1
+  revision 2
   head "https://github.com/vmware-tanzu/carvel-ytt.git"
-
-  bottle do
-    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/ytt-0.31.0_1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "6eb59a217409369bb24be60ad84fb81f55742f4ea5188c2b700d73591da2f3ed"
-  end
 
   depends_on "go" => :build
 
   def install
     ENV["CGO_ENABLED"] = "0"
-    system "go", "build", "-ldflags=-buildid=", "-trimpath", "-o", "ytt", "./cmd/ytt"
-    bin.install "ytt"
+    system "go",
+      "build",
+      "-ldflags=-buildid=",
+      "-trimpath",
+      "-o", "#{bin}/ytt",
+      "./cmd/ytt"
   end
 
   test do
