a3c8ad65ef5caed3077aa5e96e7d3b851b4c191c
Nicholas Dille <nicholas.dille@mailbox.org>
2021-03-03 11:55:20 +0100

    Added buildah

---
+++ Formula/buildah.rb
@@ -0,0 +1,30 @@
+class Buildah < Formula
+  desc "Tool that facilitates building OCI images"
+  homepage "https://github.com/containers/buildah"
+
+  url "https://github.com/containers/buildah.git",
+    tag:      "v1.19.6",
+    revision: "7aedb164287ed9c64ab38be2b3490782adadb894"
+  license "Apache-2.0"
+  head "https://github.com/containers/buildah.git"
+
+  depends_on "go" => :build
+  depends_on "go-md2man" => :build
+  depends_on "gpgme"
+  depends_on "libassuan"
+  depends_on "libseccomp"
+  depends_on "nicholasdille/tap/cni"
+  depends_on "nicholasdille/tap/runc"
+
+  def install
+    system "make", "buildah"
+    bin.install "bin/buildah"
+
+    system "make", "-C", "docs", "GOMD2MAN=go-md2man"
+    system "make", "-C", "docs", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "#{bin}/buildah", "--version"
+  end
+end
