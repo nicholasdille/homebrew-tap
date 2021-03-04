043c13570c179b9861c95a20373bb99953c90dcf
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-03-04 14:54:46 +0000

    firecracker: add 0.23.3 bottle.

--- Formula/firecracker.rb
+++ Formula/firecracker.rb
@@ -8,6 +8,11 @@ class Firecracker < Formula
   license "Apache-2.0"
   head "https://github.com/firecracker-microvm/firecracker.git"
 
+  bottle do
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/firecracker-0.23.3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "3dc808351b42689c4e068383ce3999682d195d317c2f88066c4a6bbc1f02ecb3"
+  end
+
   def install
     system "tools/devtool", "-y", "build", "--release"
     bin.install "build/cargo_target/x86_64-unknown-linux-musl/release/firecracker"
