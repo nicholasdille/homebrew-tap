1d8f2b562b05dea87cb7159650994090487ab877
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-03-04 08:36:48 +0000

    imgpkg: add 0.5.0 bottle.

--- Formula/imgpkg.rb
+++ Formula/imgpkg.rb
@@ -8,6 +8,11 @@ class Imgpkg < Formula
   license "Apache-2.0"
   head "https://github.com/vmware-tanzu/carvel-imgpkg.git"
 
+  bottle do
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/imgpkg-0.5.0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d78bf498694174b8ab486b835560f2788ddd7cad3e95d669d4f4184152d83969"
+  end
+
   depends_on "go" => :build
 
   def install
