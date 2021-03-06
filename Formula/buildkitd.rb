ffb20bf0c3a63c5402b59cf80e3b235953c46f28
Nicholas Dille <nicholas.dille@mailbox.org>
2021-03-06 21:16:37 +0100

    Build new bottles without rootless

--- Formula/buildkitd.rb
+++ Formula/buildkitd.rb
@@ -6,12 +6,8 @@ class Buildkitd < Formula
     tag:      "v0.8.2",
     revision: "9065b18ba4633c75862befca8188de4338d9f94a"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/moby/buildkit.git"
-
-  bottle do
-    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/buildkitd-0.8.2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "9bdbd7e6dca16061e8f22ea4282e18042b53ca5955b2ad6ac0274efb8dd5e4d6"
-  end
 
   depends_on "go" => :build
   depends_on "make" => :build
