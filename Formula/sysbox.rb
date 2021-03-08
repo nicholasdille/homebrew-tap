0ee03bdc0159a916b4d363676dd1ceac94a98a64
Nicholas Dille <nicholas.dille@mailbox.org>
2021-03-08 09:48:20 +0100

    Added custom download strategy to fix submodule URLs

--- Formula/sysbox.rb
+++ Formula/sysbox.rb
@@ -1,10 +1,36 @@
+class SysboxGitDownloadStrategy < GitDownloadStrategy
+  def update_submodules
+    command! "sed",
+      args:  ["-i", "-E", "s|git@github.com:|https://github.com/|", ".gitmodules"],
+      chdir: cached_location
+    command! "git",
+      args:  ["submodule", "update", "--init"],
+      chdir: cached_location
+
+    command! "sed",
+      args:  ["-i", "-E", "s|git@github.com:|https://github.com/|", "sysbox-fs/.gitmodules"],
+      chdir: cached_location
+    command! "git",
+      args:  ["submodule", "update", "--init"],
+      chdir: cached_location/"sysbox-fs"
+
+    command! "sed",
+      args:  ["-i", "-E", "s|git@github.com:|https://github.com/|", "sysbox-libs/.gitmodules"],
+      chdir: cached_location
+    command! "git",
+      args:  ["submodule", "update", "--init"],
+      chdir: cached_location/"sysbox-libs"
+  end
+end
+
 class Sysbox < Formula
   desc "Enables containers to act as virtual servers"
   homepage "https://www.nestybox.com/"
 
   url "https://github.com/nestybox/sysbox.git",
     tag:      "v0.2.1",
-    revision: "2ed540ae54e4f8fd9fff8655037de5edd38a4a56"
+    revision: "2ed540ae54e4f8fd9fff8655037de5edd38a4a56",
+    :using => SysboxGitDownloadStrategy
   license "Apache-2.0"
   head "https://github.com/nestybox/sysbox.git"
 
