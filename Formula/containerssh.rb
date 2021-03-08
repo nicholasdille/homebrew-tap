a3cd804586777d956fe36b29a56bd8690cef2245
Nicholas Dille <nicholas.dille@mailbox.org>
2021-03-08 22:55:44 +0100

    Added containerssh
    Closes #173.
    
    Signed-off-by: Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>

---
+++ Formula/containerssh.rb
@@ -0,0 +1,38 @@
+class Containerssh < Formula
+  desc "Launch containers on demand"
+  homepage "https://containerssh.io/"
+
+  url "https://github.com/ContainerSSH/ContainerSSH.git",
+    tag:      "0.4.0-PR4",
+    revision: "4eb9d631bfb7b2678c867d236aef99517db0a2d0"
+  license "MIT"
+  head "https://github.com/ContainerSSH/ContainerSSH.git"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+
+    system "go",
+      "build",
+      "-ldflags", "-s -w",
+      "-o", bin/"containerssh",
+      "./cmd/containerssh"
+
+    system "go",
+      "build",
+      "-ldflags", "-s -w",
+      "-o", bin/"containerssh-auditlog-decoder",
+      "./cmd/containerssh-auditlog-decoder"
+
+    system "go",
+      "build",
+      "-ldflags", "-s -w",
+      "-o", bin/"containerssh-testauthconfigserver",
+      "./cmd/containerssh-testauthconfigserver"
+  end
+
+  test do
+    system "#{bin}/containerssh", "--help"
+  end
+end
