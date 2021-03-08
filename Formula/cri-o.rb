ce30f41a216c728f1b559c0be97425729d631f56
Nicholas Dille <nicholas.dille@mailbox.org>
2021-03-08 22:57:00 +0100

    Added cri-o

---
+++ Formula/cri-o.rb
@@ -0,0 +1,53 @@
+class CriO < Formula
+  desc "OCI-based implementation of Kubernetes Container Runtime Interface"
+  homepage "https://cri-o.io/"
+
+  url "https://github.com/cri-o/cri-o.git",
+    tag:      "v1.19.1",
+    revision: "2af9ecf8e2494fd1bd24db12c1ca2f77dab7b5d5"
+  license "Apache-2.0"
+  head "https://github.com/cri-o/cri-o.git"
+
+  depends_on "go" => :build
+  depends_on "go-md2man" => :build
+  depends_on "make" => :build
+
+  def install
+    system "make", "binaries"
+    bin.install "bin/crio"
+    bin.install "bin/crio-status"
+    bin.install "bin/pinns"
+
+    system "make", "docs"
+    man5.install Dir["docs/*.5"]
+    man8.install Dir["docs/*.8"]
+
+    # bash completion
+    output = Utils.safe_popen_read("#{bin}/crio", "complete", "bash")
+    (bash_completion/"crio").write output
+
+    # fish completion
+    output = Utils.safe_popen_read("#{bin}/crio", "complete", "fish")
+    (zsh_completion/"crio.fish").write output
+
+    # zsh completion
+    output = Utils.safe_popen_read("#{bin}/crio", "complete", "zsh")
+    (zsh_completion/"_crio").write output
+
+    # bash completion
+    output = Utils.safe_popen_read("#{bin}/crio-status", "complete", "bash")
+    (bash_completion/"crio-status").write output
+
+    # fish completion
+    output = Utils.safe_popen_read("#{bin}/crio-status", "complete", "fish")
+    (zsh_completion/"crio-status.fish").write output
+
+    # zsh completion
+    output = Utils.safe_popen_read("#{bin}/crio-status", "complete", "zsh")
+    (zsh_completion/"_crio-status").write output
+  end
+
+  test do
+    system "#{bin}/conmon", "--version"
+  end
+end
