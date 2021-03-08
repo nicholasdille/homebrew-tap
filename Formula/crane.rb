cdd0b088e220b4d851500a5c27f613e5fa93d8d0
Nicholas Dille <nicholas.dille@mailbox.org>
2021-03-08 22:56:26 +0100

    Added crane

---
+++ Formula/crane.rb
@@ -0,0 +1,26 @@
+class Crane < Formula
+  desc "Working with container registries"
+  homepage "https://github.com/google/go-containerregistry/blob/main/cmd/crane/README.md"
+
+  url "https://github.com/google/go-containerregistry.git",
+    tag:      "v0.4.1",
+    revision: "efb2d62d93a7705315b841d0544cb5b13565ff2a"
+  license "Apache-2.0"
+  head "https://github.com/google/go-containerregistry.git"
+
+  depends_on "go" => :build
+
+  def install
+    pkg = "github.com/google/go-containerregistry"
+    ENV["CGO_ENABLED"] = "0"
+    system "go",
+      "build",
+      "-ldflags", "-s -w -X #{pkg}/cmd/crane/cmd.Version=#{version}",
+      "-o", bin/"crane",
+      "./cmd/crane"
+  end
+
+  test do
+    system "#{bin}/crane", "version"
+  end
+end
