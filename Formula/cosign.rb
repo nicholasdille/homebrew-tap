6fde7a53df9d1137ea3666e85f908ff89cfd60e4
Nicholas Dille <nicholas.dille@mailbox.org>
2021-03-11 15:06:08 +0100

    Added cosign and rekor CLI

---
+++ Formula/cosign.rb
@@ -0,0 +1,26 @@
+class Cosign < Formula
+  desc "Container Signing"
+  homepage "https://sigstore.dev/"
+
+  url "https://github.com/sigstore/cosign.git",
+    tag:      "main",
+    revision: "f74fac540531411764589d72467ad6641e402971"
+  version "main"
+  license "Apache-2.0"
+  head "https://github.com/sigstore/cosign.git"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    system "go",
+      "build",
+      "-ldflags", "-s -w",
+      "-o", bin/"cosign",
+      "./cmd"
+  end
+
+  test do
+    system bin/"cosign", "--help"
+  end
+end
---
+++ Formula/rekor.rb
@@ -0,0 +1,25 @@
+class Rekor < Formula
+  desc "Signature Transparency Log"
+  homepage "https://rekor.dev/"
+
+  url "https://github.com/sigstore/rekor.git",
+    tag:      "v0.1.1",
+    revision: "3c6ce1678a9b4e02e98be671fe901490d071fcb8"
+  license "Apache-2.0"
+  head "https://github.com/sigstore/rekor.git"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    system "go",
+      "build",
+      "-ldflags", "-s -w",
+      "-o", bin/"rekor",
+      "./cmd/cli"
+  end
+
+  test do
+    system bin/"rekor", "--help"
+  end
+end
