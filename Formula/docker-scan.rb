7f8198cdf1b2f0c2f35647f10e3beb09f9d7d30c
Nicholas Dille <nicholas.dille@mailbox.org>
2021-03-11 17:33:26 +0100

    Added docker-scan
    Closes #188.
    
    Signed-off-by: Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>

---
+++ Formula/docker-scan.rb
@@ -0,0 +1,31 @@
+class DockerScan < Formula
+  desc "CLI to run vulnerability detection on your Dockerfiles and Docker images"
+  homepage "https://github.com/docker/scan-cli-plugin"
+
+  url "https://github.com/docker/scan-cli-plugin.git",
+    tag:      "v0.5.0",
+    revision: "5a092666da3fd585d3095c62248c04094b7a4cad"
+  license "Apache-2.0"
+  head "https://github.com/docker/scan-cli-plugin.git"
+
+  depends_on "go" => :build
+  depends_on "make" => :build
+
+  def install
+    system "make", "-f", "builder.Makefile", "build", "PLATFORM_BINARY=docker-scan"
+    bin.install "bin/docker-scan"
+  end
+
+  def caveats
+    <<~EOS
+      You should create a symlink to enable the Docker CLI plugin:
+
+      mkdir -p $HOME/.docker/cli-plugins
+      ln -s #{HOMEBREW_PREFIX}/bin/docker-scan $HOME/.docker/cli-plugins
+    EOS
+  end
+
+  test do
+    system "whereis", "docker-scan"
+  end
+end
