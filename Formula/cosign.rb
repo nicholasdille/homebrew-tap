d5d5ba29176a7e416196a5542ec68eb25c874b68
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-03-11 16:53:36 +0000

    cosign: add 0.0.0 bottle.

--- Formula/cosign.rb
+++ Formula/cosign.rb
@@ -8,6 +8,11 @@ class Cosign < Formula
   license "Apache-2.0"
   head "https://github.com/sigstore/cosign.git"
 
+  bottle do
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/cosign-0.0.0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2e70353632bdfa57c08437ab58212f7e46aa413e1172a4689cf7e212bf5bf227"
+  end
+
   depends_on "go" => :build
 
   def install
