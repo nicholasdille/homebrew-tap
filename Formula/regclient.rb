92b973b2839e101ac416fb75045230e821bde0f3
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-04-12 09:30:34 +0000

    regclient: update 0.3.0_1 bottle.

--- Formula/regclient.rb
+++ Formula/regclient.rb
@@ -10,8 +10,9 @@ class Regclient < Formula
   head "https://github.com/regclient/regclient.git"
 
   bottle do
-    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/regclient-0.3.0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "27bb2be30e54081c7ddb4db35d14febd8e3140610859b7a35e834eab1651384e"
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/regclient-0.3.0_1"
+    sha256 cellar: :any_skip_relocation, catalina:     "7bbc20372af5b878266980d4dbf385a9441ce9a74dd0d892a9917a7a546970d8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8cee02bd8c9bb09e52a8cdac87ceff4375cc46717b16983e2501ebb74b3ebb8e"
   end
 
   depends_on "go" => :build
