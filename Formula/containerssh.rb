8e7fe318310cb1f5bb36bb98e746a290c97d0ba6
Nicholas Dille <nicholas.dille@mailbox.org>
2021-04-12 09:38:03 +0200

    Bump from nicholasdille/renovate-homebrew#57

--- Formula/containerssh.rb
+++ Formula/containerssh.rb
@@ -3,15 +3,10 @@ class Containerssh < Formula
   homepage "https://containerssh.io/"
 
   url "https://github.com/ContainerSSH/ContainerSSH.git",
-    tag:      "0.4.0-PR4",
-    revision: "4eb9d631bfb7b2678c867d236aef99517db0a2d0"
+    tag:      "v0.4.0",
+    revision: "c598ba7448f45611133882bdc4467aa104e73dd8"
   license "MIT"
   head "https://github.com/ContainerSSH/ContainerSSH.git"
-
-  bottle do
-    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/containerssh-4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ca9583acaa938cc4f1957e5cf76a0c22d1d1dd021ff6f657ea96f27445dd4152"
-  end
 
   depends_on "go" => :build
 
