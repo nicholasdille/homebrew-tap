6fcc823804f0d1316ba87b93798c1533e92a97b8
Nicholas Dille <nicholas.dille@mailbox.org>
2021-04-12 09:01:14 +0200

    fuse-overlayfs-bin 1.5.0
    Closes #227.
    
    Signed-off-by: Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>

--- Formula/fuse-overlayfs-bin.rb
+++ Formula/fuse-overlayfs-bin.rb
@@ -2,9 +2,9 @@ class FuseOverlayfsBin < Formula
   desc "Simple deployment tool focused on the concept of Kubernetes application"
   homepage "https://github.com/containers/fuse-overlayfs"
 
-  url "https://github.com/containers/fuse-overlayfs/releases/download/v1.4.0/fuse-overlayfs-x86_64"
-  version "1.4.0"
-  sha256 "692cb1d5917159c09ee2fe005ad136b2ca75469fc52cd07d67361aca88a5a8f3"
+  url "https://github.com/containers/fuse-overlayfs/releases/download/v1.5.0/fuse-overlayfs-x86_64"
+  version "1.5.0"
+  sha256 "53e54b2febf39ba6e67018294a7162bd6b4d18cb544ed7aff54c29ffb2791606"
   license "GPL-3.0-only"
 
   bottle :unneeded
