fc18ffa4ceeac7fead0264b4bd562cefae486b0d
Nicholas Dille <nicholas.dille@mailbox.org>
2021-04-13 14:05:34 +0200

    Added porter
    Closes #244.
    
    Signed-off-by: Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>

---
+++ Formula/porter.rb
@@ -0,0 +1,28 @@
+class Porter < Formula
+  desc "Your application and everything you need to deploy. Together"
+  homepage "https://porter.sh/"
+
+  url "https://github.com/getporter/porter.git",
+    tag:      "v0.37.2",
+    revision: "2629955905a65e4468369ab5f7e377f13bfd9b1e"
+  license "Apache-2.0"
+  head "https://github.com/getporter/porter.git"
+
+  option "with-btrfs", "Support BTRFS, requires libbtrfs-dev"
+  option "without-devmapper", "Support device mapper"
+  option "without-cri", "Support CRI"
+
+  depends_on "go" => :build
+  depends_on "make" => :build
+  depends_on "packr" => :build
+
+  def install
+    ENV["GO111MODULE"] = "auto"
+    system "make", "build-porter"
+    bin.install "bin/porter"
+  end
+
+  test do
+    system bin/"porter", "--version"
+  end
+end
