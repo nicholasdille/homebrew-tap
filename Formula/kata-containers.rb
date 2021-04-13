b4b63e5fab82954f40b80bd96dbd83ccd2afb2da
Nicholas Dille <nicholas.dille@mailbox.org>
2021-04-13 11:50:17 +0200

    Fixed style

--- Formula/kata-containers.rb
+++ Formula/kata-containers.rb
@@ -8,25 +8,24 @@ class KataContainers < Formula
   license "Apache-2.0"
   head "https://github.com/kata-containers/kata-containers.git"
 
+  depends_on "gcc" => :build
   depends_on "go" => :build
   depends_on "make" => :build
   depends_on "rust" => :build
-  depends_on "gcc" => :build
 
   def install
     dir = buildpath/"src/github.com/kata-containers/kata-containers"
     dir.install (buildpath/"").children
     cd dir do
       ENV["GOPATH"] = buildpath
-      #https://github.com/kata-containers/kata-containers/blob/main/docs/Developer-Guide.md#initial-setup
+      # https://github.com/kata-containers/kata-containers/blob/main/docs/Developer-Guide.md#initial-setup
       system "make"
-      #kata-runtime
-      #containerd-shim-kata-v2
-      #configuration.toml
+      # kata-runtime
+      # containerd-shim-kata-v2
+      # configuration.toml
     end
   end
 
-  test do
-    #
-  end
+  # test do
+  # end
 end
