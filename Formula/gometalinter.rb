cb9e95ed2f556e3357323c60d6a0949a005f74fa
Nicholas Dille <nicholas.dille@mailbox.org>
2021-04-13 14:02:11 +0200

    Added distribution

---
+++ Formula/gometalinter.rb
@@ -0,0 +1,42 @@
+class Gometalinter < Formula
+  desc "Concurrently run a whole bunch of linters"
+  homepage "https://github.com/alecthomas/gometalinter"
+
+  url "https://github.com/alecthomas/gometalinter/releases/download/v3.0.0/gometalinter-3.0.0-linux-amd64.tar.gz"
+  sha256 "2cab9691fa1f94184ea1df2b69c00990cdf03037c104e6a9deab6815cdbe6a77"
+  license "MIT"
+  head "https://github.com/alecthomas/gometalinter.git"
+
+  bottle :unneeded
+
+  def install
+    bin.install "gometalinter"
+    bin.install "gocyclo"
+    bin.install "nakedret"
+    bin.install "misspell"
+    bin.install "gosec"
+    bin.install "golint"
+    bin.install "ineffassign"
+    bin.install "goconst"
+    bin.install "errcheck"
+    bin.install "maligned"
+    bin.install "unconvert"
+    bin.install "dupl"
+    bin.install "structcheck"
+    bin.install "varcheck"
+    bin.install "safesql"
+    bin.install "deadcode"
+    bin.install "lll"
+    bin.install "goimports"
+    bin.install "gotype"
+    bin.install "staticcheck"
+    bin.install "interfacer"
+    bin.install "unparam"
+    bin.install "gochecknoinits"
+    bin.install "gochecknoglobals"
+  end
+
+  test do
+    system bin/"gometalinter", "--version"
+  end
+end
