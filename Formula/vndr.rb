e47e30ed0948d9fdc1ccc96a48fb0d6e23f98b94
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-04-14 07:37:42 +0000

    vndr: add 0.1.2 bottle.

--- Formula/vndr.rb
+++ Formula/vndr.rb
@@ -8,6 +8,12 @@ class Vndr < Formula
   license "Apache-2.0"
   head "https://github.com/LK4D4/vndr.git"
 
+  bottle do
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/vndr-0.1.2"
+    sha256 cellar: :any_skip_relocation, catalina:     "028b4552b2eeb29e52bc4c3c0ca63feacbb4b6e233b3348b5d0bdd75c78c77e9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "086d6e1aba7c462ac586e5dec9ba52e599a597366e91c6b843f7c85adb39e838"
+  end
+
   depends_on "go" => :build
 
   def install
