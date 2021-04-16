1e113b39d9fc29052004bb889de6031abeb297c1
Nicholas Dille <nicholas.dille@mailbox.org>
2021-04-16 10:31:24 +0200

    Fixes

--- Formula/quark.rb
+++ Formula/quark.rb
@@ -2,20 +2,27 @@ class Quark < Formula
   desc "Secure container runtime with OCI interface"
   homepage "https://github.com/QuarkContainer/Quark"
 
-  url "https://github.com/QuarkContainer/Quark/archive/main.zip"
-  sha256 "5e90837a3d2f41bad0243d6a3410a7b85f9664a0fe768a2edde547e6db0c1bc9"
-  version "main"
+  url "https://github.com/QuarkContainer/Quark.git",
+    revision: "2021648c1cdf5b35a9812583cba06ada180f3032"
+  version "0.0.0"
   license "Apache-2.0"
   head "https://github.com/QuarkContainer/Quark.git"
 
-  depends_on "rust" => :build
   depends_on "make" => :build
+  depends_on "rust" => :build
 
   def install
     system "make", "release"
+    bin.install "build/qkernel.bin"
+    bin.install "target/release/quark"
+    bin.install "vdso/vdso.so"
+  end
+
+  def post_install
+    (etc/"quark").install "config.json"
   end
 
   test do
-    #
+    system bin/"qvisor", "--help"
   end
 end
