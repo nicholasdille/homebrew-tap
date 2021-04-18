871fd32730a02debdffb1d18480c79c6965f186d
Nicholas Dille <nicholas.dille@mailbox.org>
2021-04-18 18:06:51 +0200

    Some progress

--- Formula/kata-containers.rb
+++ Formula/kata-containers.rb
@@ -12,17 +12,25 @@ class KataContainers < Formula
   depends_on "go" => :build
   depends_on "make" => :build
   depends_on "rust" => :build
+  depends_on "yq" => :build
+  depends_on :linux
 
   def install
     dir = buildpath/"src/github.com/kata-containers/kata-containers"
+    (buildpath/"src").rename "src2"
     dir.install (buildpath/"").children
-    cd dir do
+    (dir/"src2").rename dir/"src"
+    cd dir/"src/runtime" do
       ENV["GOPATH"] = buildpath
-      # https://github.com/kata-containers/kata-containers/blob/main/docs/Developer-Guide.md#initial-setup
-      system "make"
-      # kata-runtime
-      # containerd-shim-kata-v2
+
+      # https://github.com/kata-containers/kata-containers/blob/main/docs/Developer-Guide.md#build-and-install-the-kata-containers-runtime
+      system "make", "PREFIX=#{prefix}"
+      bin.install "kata-runtime"
+      bin.install "kata-monitor"
+      bin.install "kata-netmon"
+      bin.install "containerd-shim-kata-v2"
       # configuration.toml
+      bash_completion.install "src/runtime/data/completions/bash/kata-runtime"
     end
   end
 
