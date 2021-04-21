7f1c023f7a3d53f7f8a4b78526846e527e6af018
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-04-21 09:55:14 +0000

    vcluster: add 0.1.0 bottle.

--- Formula/vcluster.rb
+++ Formula/vcluster.rb
@@ -8,6 +8,12 @@ class Vcluster < Formula
   license "Apache-2.0"
   head "https://github.com/loft-sh/vcluster.git"
 
+  bottle do
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/vcluster-0.1.0"
+    sha256 cellar: :any_skip_relocation, catalina:     "11552f5eb7ff641b41fb755673abb8a26ba68ac63d31183571eb05bb99cf4678"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "be552b82a94a216c61e21939a34f77a08d3d704b61992923be4818ea69ad9350"
+  end
+
   depends_on "go" => :build
   depends_on arch: :x86_64
 
