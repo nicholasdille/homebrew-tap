c29309512636735c41a827b2612a45f5b4dd906e
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-04-21 06:46:43 +0000

    vendir: update 0.19.0 bottle.

--- Formula/vendir.rb
+++ Formula/vendir.rb
@@ -9,8 +9,9 @@ class Vendir < Formula
   head "https://github.com/vmware-tanzu/carvel-vendir.git"
 
   bottle do
-    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/vendir-0.18.0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "bd9aac1d424adc13c35fffca864bbc9c08a987bea524e6ec7b7a78094cb8e068"
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/vendir-0.19.0"
+    sha256 cellar: :any_skip_relocation, catalina:     "01f5b7337a66d2b74219cbf17189881143cdf5c13011d2952cbbdf9c64081ef1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "a2f31113989994bdf4f5f91b0ae31c8ccba1e3f43c7de53f2cb4e950592095e7"
   end
 
   depends_on "go" => :build
