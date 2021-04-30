3df3b15e42a4534130814024cd000f8238df2ce7
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-04-30 09:18:24 +0000

    buildkitd: update 0.8.3 bottle.

--- Formula/buildkitd.rb
+++ Formula/buildkitd.rb
@@ -9,8 +9,8 @@ class Buildkitd < Formula
   head "https://github.com/moby/buildkit.git"
 
   bottle do
-    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/buildkitd-0.8.2_1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "4e08dc487351284f2e1e7fd5869109e6aa0a086b9847431d1edd8867f386f1e5"
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/buildkitd-0.8.3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f47f1d55126577897a90dd20e367e4c320ecd7cb84d36213ba395bd886c4908a"
   end
 
   depends_on "go" => :build
