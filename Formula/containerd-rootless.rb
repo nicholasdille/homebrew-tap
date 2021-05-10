24a10d978f54effa55875f7127ae2dc7248225ef
Nicholas Dille <nicholas.dille@mailbox.org>
2021-05-10 17:38:41 +0200

    Fixed rootless containerd

--- Formula/containerd-rootless.rb
+++ Formula/containerd-rootless.rb
@@ -6,6 +6,7 @@ class ContainerdRootless < Formula
     tag:      "v0.8.1",
     revision: "e1601447477c38ceb46c9c88418af399f79b1d6a"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/containerd/containerd.git"
 
   bottle do
@@ -22,7 +23,7 @@ class ContainerdRootless < Formula
     bin.install "extras/rootless/containerd-rootless.sh"
 
     (buildpath/"containerd.yml").write <<~EOS
-      cmd: #{HOMEBREW_PREFIX}/bin/containerd-rootless.sh
+      cmd: #{HOMEBREW_PREFIX}/bin/containerd-rootless.sh --config #{etc}/containerd/config.toml
       env:
         XDG_RUNTIME_DIR: #{var}/run/containerd
         XDG_DATA_HOME: #{var}/lib/containerd
@@ -38,6 +39,38 @@ class ContainerdRootless < Formula
         timestamp: true
     EOS
     (etc/"immortal").install "containerd.yml"
+    
+    (buildpath/"config.toml").write Utils.safe_popen_read(
+      HOMEBREW_PREFIX/"bin/containerd",
+      "config",
+      "default"
+    )
+    system "sed",
+      "-i",
+      "-E",
+      "s|(/var)?/run/containerd|/home/linuxbrew/.linuxbrew/var/run/containerd|",
+      buildpath/"config.toml"
+    system "sed",
+      "-i",
+      "-E",
+      "s|/var/lib/containerd|/home/linuxbrew/.linuxbrew/var/lib/containerd|",
+      buildpath/"config.toml"
+    system "sed",
+      "-i",
+      "-E",
+      "s|/opt/cni/bin|/home/linuxbrew/.linuxbrew/bin|",
+      buildpath/"config.toml"
+    system "sed",
+      "-i",
+      "-E",
+      "s|/etc/cni/net.d|/home/linuxbrew/.linuxbrew/etc/cni/net.d|",
+      buildpath/"config.toml"
+    system "sed",
+      "-i",
+      "-E",
+      "s|/etc/containerd|/home/linuxbrew/.linuxbrew/etc/containerd|",
+      buildpath/"config.toml"
+    (etc/"containerd").install "config.toml"
   end
 
   def post_install
@@ -47,12 +80,14 @@ class ContainerdRootless < Formula
 
   def caveats
     <<~EOS
-      TODO: brew immortal
-      TODO: nsenter -U --preserve-credentials -m -n -t $(cat /home/linuxbrew/.linuxbrew/var/run/rootlesskit/containerd/child_pid)
-      TODO: export CONTAINERD_ADDRESS=/home/linuxbrew/.linuxbrew/var/run/containerd/containerd.sock
-      TODO: export CONTAINERD_SNAPSHOTTER=native
-      TODO: ctr images pull docker.io/library/alpine:latest
-      TODO: ctr run -t --rm --fifo-dir /tmp/foo-fifo --cgroup "" docker.io/library/alpine:latest foo
+      - You can now run rootless buildkitd using immortal
+      - It is recommended to use nerdctl
+      - If using ctr:
+          $ nsenter -U --preserve-credentials -m -n -t $(cat #{var}/run/containerd/containerd-rootless/child_pid)
+          $ export CONTAINERD_ADDRESS=#{var}/run/containerd/containerd.sock
+          $ export CONTAINERD_SNAPSHOTTER=native
+          $ ctr images pull docker.io/library/alpine:latest
+          $ ctr run -t --rm --fifo-dir /tmp/foo-fifo --cgroup "" docker.io/library/alpine:latest foo
     EOS
   end
 
