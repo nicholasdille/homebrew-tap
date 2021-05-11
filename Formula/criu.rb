d41751246b7d54cd75e0efb370b6d26bb52debfe
Nicholas Dille <nicholas.dille@mailbox.org>
2021-05-11 09:24:24 +0200

    Added criu

---
+++ Formula/criu.rb
@@ -0,0 +1,26 @@
+class Criu < Formula
+  desc "Checkpoint/Restore tool"
+  homepage "http://criu.org/"
+
+  url "https://github.com/checkpoint-restore/criu.git",
+    tag:      "v3.15",
+    revision: "f68da4a86fee62af216e9e7520f4916aa29e797b"
+  license ""
+  head "https://github.com/checkpoint-restore/criu.git"
+
+  depends_on "asciidoc" => :build
+  depends_on "libnet" => :build
+  depends_on "pkg-config" => :build
+  depends_on "protobuf-c" => :build
+  depends_on "xmlto" => :build
+  depends_on :linux
+
+  def install
+    system "ln", "-sf", "#{HOMEBREW_PREFIX}/include/google/protobuf/descriptor.proto", "#{buildpath}/images/google/protobuf/"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system bin/"criu", "--version"
+  end
+end
