0d52bbdd46c8a99b5d6aaca988059cac5d6fc21f
Nicholas Dille <nicholas.dille@mailbox.org>
2021-05-11 16:34:47 +0200

    Added npiperelay

---
+++ Formula/npiperelay-bin.rb
@@ -0,0 +1,19 @@
+class NpiperelayBin < Formula
+  desc "Access Windows named pipes from WSL"
+  homepage "https://docs.docker.com/compose/"
+
+  url "https://github.com/jstarks/npiperelay/releases/download/v0.1.0/npiperelay_windows_amd64.zip"
+  sha256 "6b9ef61ffd17c03507a9a3d54d815dceb3dae669ac67fc3bf4225d1e764ce5f6"
+  license "MIT"
+  head "https://github.com/jstarks/npiperelay.git"
+
+  bottle :unneeded
+
+  def install
+    bin.install "npiperelay.exe"
+  end
+
+  test do
+    system bin/"npiperelay.exe", "version"
+  end
+end
