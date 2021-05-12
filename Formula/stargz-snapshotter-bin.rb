17ccb57700f67eb4a5227676727577d7d7239d65
Nicholas Dille <nicholas.dille@mailbox.org>
2021-05-12 10:12:37 +0200

    stargz-snapshotter-bin 0.6.0
    Closes #360.
    
    Signed-off-by: Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>

--- Formula/stargz-snapshotter-bin.rb
+++ Formula/stargz-snapshotter-bin.rb
@@ -2,9 +2,9 @@ class StargzSnapshotterBin < Formula
   desc "Fast docker image distribution plugin for containerd, based on CRFS/stargz"
   homepage "https://github.com/containerd/containerd/issues/3731"
 
-  url "https://github.com/containerd/stargz-snapshotter/releases/download/v0.5.0/stargz-snapshotter-v0.5.0-linux-amd64.tar.gz"
-  version "0.5.0"
-  sha256 "a800f1ef707443260df6ea2b0627edb404ecad67bbbefea81b997c4499555b02"
+  url "https://github.com/containerd/stargz-snapshotter/releases/download/v0.6.0/stargz-snapshotter-v0.6.0-linux-amd64.tar.gz"
+  version "0.6.0"
+  sha256 "893b80c80780d508eae311278e7922765bb52fe67c658dcb2215d168e54ca992"
   license "Apache-2.0"
 
   bottle :unneeded
