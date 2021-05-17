87ba15162e5bb0b3e4f2f79ef532d1cc55c0bac3
Nicholas Dille <nicholas.dille@mailbox.org>
2021-05-17 08:25:23 +0200

    trivy 0.18.1
    Closes #361.
    
    Signed-off-by: Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>

--- Formula/trivy.rb
+++ Formula/trivy.rb
@@ -3,8 +3,8 @@ class Trivy < Formula
   homepage "https://github.com/aquasecurity/trivy"
 
   url "https://github.com/aquasecurity/trivy.git",
-    tag:      "v0.17.2",
-    revision: "415e1d8ea3832b3f1807884b7c969b4d030d8098"
+    tag:      "v0.18.1",
+    revision: "eaf2da20a690874570509e9924e2d5f1680c813d"
   license "Apache-2.0"
   head "https://github.com/aquasecurity/trivy.git"
 
