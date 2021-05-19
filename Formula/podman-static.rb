b1533e90fb072bb70df26d8b5c60c43317076404
Nicholas Dille <nicholas.dille@mailbox.org>
2021-05-19 10:34:53 +0200

    Re-added deleted files

---
+++ Formula/podman-static.rb
@@ -0,0 +1,78 @@
+class PodmanStatic < Formula
+    desc "Tool for managing OCI containers and pods"
+    homepage "https://podman.io/"
+  
+    url "https://github.com/containers/podman.git",
+      tag:      "v3.1.2",
+      revision: "51b8ddbc22cf5b10dd76dd9243924aa66ad7db39"
+    license "Apache-2.0"
+    revision 2
+    head "https://github.com/containers/podman.git"
+  
+    bottle do
+      root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/podman-static-3.1.2_2"
+      sha256 cellar: :any_skip_relocation, x86_64_linux: "5d3b937320cc0580a7bb3f092a8b2689527ca77c303bcbdb4b0a88274d7b9fd9"
+    end
+  
+    depends_on "go-md2man" => :build
+    depends_on "make" => :build
+    depends_on :linux
+  
+    def install
+      # Build base from https://github.com/NixOS/docker
+      system "docker",
+        "build",
+        "--tag", "nix",
+        "github.com/NixOS/docker"
+  
+      # Create Dockerfile
+      (buildpath/"Dockerfile").write <<~EOS
+        FROM nix
+        RUN apk update \
+         && apk add \
+                bash \
+                make \
+                git \
+                go
+      EOS
+  
+      # Build custom image
+      system "docker",
+        "build",
+        "--tag", "podman",
+        "."
+  
+      # Run build
+      system "docker",
+        "run",
+        "--interactive",
+        "--rm",
+        "--mount", "type=bind,src=#{buildpath},dst=/src",
+        "--workdir", "/src",
+        "podman",
+        "make", "static"
+  
+      # Fix permission
+      system "docker",
+        "run",
+        "--interactive",
+        "--rm",
+        "--mount", "type=bind,src=#{buildpath},dst=/src",
+        "--workdir", "/src",
+        "alpine",
+        "chown", "-R", "#{Process.uid}:#{Process.gid}", "."
+  
+      bin.install "bin/podman"
+  
+      system "make", "docs"
+      man1.install Dir["docs/build/man/*.1"]
+  
+      bash_completion.install "completions/bash/podman"
+      zsh_completion.install "completions/zsh/_podman"
+    end
+  
+    test do
+      assert_match "podman version #{version}", shell_output("#{bin}/podman -v")
+    end
+  end
+  
