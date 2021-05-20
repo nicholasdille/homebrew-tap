f081d1372f73387666ec6bb2294a54c80fb43081
Nicholas Dille <nicholas.dille@mailbox.org>
2021-05-20 11:14:02 +0200

    Added dependencies to fix XML validation

--- Formula/criu.rb
+++ Formula/criu.rb
@@ -9,6 +9,8 @@ class Criu < Formula
   head "https://github.com/checkpoint-restore/criu.git"
 
   depends_on "asciidoc" => :build
+  depends_on "docbook" => :build
+  depends_on "docbook-xsl" => :build
   depends_on "libnet" => :build
   depends_on "pkg-config" => :build
   depends_on "protobuf-c" => :build
