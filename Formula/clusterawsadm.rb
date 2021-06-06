4a3d3d375f024caf581cc5b077abd4b56e44c1c3
Nicholas Dille <nicholas.dille@mailbox.org>
2021-06-06 21:43:49 +0200

    clusterawsadm 0.6.6

--- Formula/clusterawsadm.rb
+++ Formula/clusterawsadm.rb
@@ -2,8 +2,8 @@ class Clusterawsadm < Formula
   desc "Kubernetes Cluster API Provider AWS provides consistent deployment and day 2"
   homepage "https://cluster-api-aws.sigs.k8s.io/"
   url "https://github.com/kubernetes-sigs/cluster-api-provider-aws.git",
-    tag:      "v0.6.5",
-    revision: "c7c8ae1ee071afbb33c3b45b0f9adbea149581ce"
+    tag:      "v0.6.6",
+    revision: "4ca623be01b48cc6a28f83d600b582aa7417498e"
   license "Apache-2.0"
   head "https://github.com/kubernetes-sigs/cluster-api-provider-aws.git"
 
