d56c4243ee31fcb568ad1a8fb7acdddf9fc6dbdb
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-06-15 06:59:50 +0000

    slirp4netns: update 1.1.11 bottle.

--- Formula/slirp4netns.rb
+++ Formula/slirp4netns.rb
@@ -9,8 +9,8 @@ class Slirp4netns < Formula
   head "https://github.com/rootless-containers/slirp4netns.git"
 
   bottle do
-    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/slirp4netns-1.1.10"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "c334c4eaf719522f02a95bac0a0002c8bd12fea7fa8c3e6d9b7d54229472e0c6"
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/slirp4netns-1.1.11"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f732961082ca787d58c4c23979d82ccd486b0a41546cf542a1cd0c7f6f7f55a6"
   end
 
   depends_on "autoconf" => :build
