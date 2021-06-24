96f00c2a726e5208d3b767d98fa7e81694d22f4f
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-06-24 21:01:49 +0000

    containers-image: add 5.13.2 bottle.

--- Formula/containers-image.rb
+++ Formula/containers-image.rb
@@ -8,6 +8,12 @@ class ContainersImage < Formula
   license "Apache-2.0"
   head "https://github.com/containers/image.git"
 
+  bottle do
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/containers-image-5.13.2"
+    sha256 cellar: :any_skip_relocation, catalina:     "d559afc83be1197603abd9b727bb49b9c934a229767472cb2e6fd98548df73af"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0a179325eeb6f8d584a28a71aa90a49b00354f2ca112bdc3d9db8f22c699d90b"
+  end
+
   depends_on "go-md2man" => :build
   depends_on "make" => :build
 
