d52c8aaeb3fa29a439aad72263a9b7b4959b7d3c
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-06-24 21:01:49 +0000

    podman-remote: update 3.2.1_2 bottle.

--- Formula/podman-remote.rb
+++ Formula/podman-remote.rb
@@ -10,10 +10,9 @@ class PodmanRemote < Formula
   head "https://github.com/containers/podman.git"
 
   bottle do
-    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/podman-remote-3.2.1_1"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, catalina:     "821181072214982ea678062c6426acfa3b1b5337636157b5a77c9e70b0a8c618"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ad1135e5a81c7d19acf23eca0b044ee6c2b169022983caebcb65a216e88bc40c"
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/podman-remote-3.2.1_2"
+    sha256 cellar: :any_skip_relocation, catalina:     "ffd79c098a6a72a6918c1b96c8490c2c100866e28c0eb0871f2f222b65bee2f9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "af5bdc01c2584f20ed9ff86200b7f9f6f559d4835e9f04baf90c1295ca035d39"
   end
 
   depends_on "go" => :build
