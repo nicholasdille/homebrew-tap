fb9d90aa8238dffe4b5b4e2afa42ba0a80c008cb
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-06-25 11:52:18 +0000

    docker-proxy: add 20.10.7 bottle.

--- Formula/docker-proxy.rb
+++ Formula/docker-proxy.rb
@@ -8,6 +8,11 @@ class DockerProxy < Formula
   license "Apache-2.0"
   head "https://github.com/moby/libnetwork.git"
 
+  bottle do
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/docker-proxy-20.10.7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d70113a134fcef5d02d08d10d1244da60aa3601d19549afa4ee9fc85a7a7b6d1"
+  end
+
   depends_on "go" => :build
   depends_on "make" => :build
   depends_on "pkg-config" => :build
