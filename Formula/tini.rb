c131594e3a5c8a71d5d34d79b81340f6db86f6fc
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-06-25 11:52:18 +0000

    tini: add 0.19.0 bottle.

--- Formula/tini.rb
+++ Formula/tini.rb
@@ -8,6 +8,11 @@ class Tini < Formula
   license "MIT"
   head "https://github.com/krallin/tini.git"
 
+  bottle do
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/tini-0.19.0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "fba61c8a752fa5f43cdcae4affa4169c010027a6f540a7b62f49ed8c8da1794a"
+  end
+
   depends_on "cmake" => :build
   depends_on "gcc" => :build
   depends_on "make" => :build
