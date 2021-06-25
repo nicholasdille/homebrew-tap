86ec8195fa4a2e05102678aab64556cd489d18f3
Nicholas Dille <nicholas.dille@mailbox.org>
2021-06-25 08:53:14 +0200

    Added CNI plugin dnsname
    Closes #454.
    
    Signed-off-by: Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>

---
+++ Formula/cni-dnsname.rb
@@ -0,0 +1,23 @@
+class CniDnsname < Formula
+  desc "Name resolution for containers"
+  homepage "https://github.com/containers/dnsname"
+
+  url "https://github.com/containers/dnsname.git",
+    tag:      "v1.1.1",
+    revision: "c654c95366ac5f309ca3e5727c9b858864247328"
+  license "Apache-2.0"
+  head "https://github.com/containers/dnsname.git"
+
+  depends_on "go" => :build
+  depends_on "make" => :build
+  depends_on "dnsmasq"
+
+  def install
+    system "make", "all"
+    bin.install "bin/dnsname"
+  end
+
+  test do
+    system bin/"dnsname"
+  end
+end
