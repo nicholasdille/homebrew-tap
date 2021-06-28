396954eb2a5e57819701ef358e5294613a3439c7
Nicholas Dille <nicholas.dille@mailbox.org>
2021-06-28 09:05:27 +0200

    Version bump

--- Formula/gvisor.rb
+++ Formula/gvisor.rb
@@ -3,8 +3,8 @@ class Gvisor < Formula
   homepage "https://gvisor.dev/"
 
   url "https://github.com/google/gvisor.git",
-    tag:      "release-20210503.0",
-    revision: "7cafac9f42a8355e7192b57fcc8d41e31d836c53"
+    tag:      "release-20210614.0",
+    revision: "3cf1644a3b4266979b7dda8b6291edf19264fa19"
   license "Apache-2.0"
   head "https://github.com/google/gvisor.git"
 
