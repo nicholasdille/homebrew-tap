e55c966ef7421f885fbcca6209db9836b6ff1cb6
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-07-07 08:48:04 +0000

    docker-compose-cli: update 2.0.0-beta.5 bottle.

--- Formula/docker-compose-cli.rb
+++ Formula/docker-compose-cli.rb
@@ -9,9 +9,9 @@ class DockerComposeCli < Formula
   head "https://github.com/docker/compose-cli.git"
 
   bottle do
-    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/docker-compose-cli-2.0.0-beta.4_1"
-    sha256 cellar: :any_skip_relocation, catalina:     "f3c1f4b029c5c29f1e0bfa58c02b1b71371d29f26a66b9d9e149b137dbf98904"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "148dbc1da713ff2d9591ceb6b3e1c497ddffba8c5227401525e36a4e788c9278"
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/docker-compose-cli-2.0.0-beta.5"
+    sha256 cellar: :any_skip_relocation, catalina:     "ea28d96b6d89004c26c46cc604e2d8661a55da9f1841aba9c82a90444356d68e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "a90cae17e0acf690b33e67c5b9c547b9e1ee0cbe8b6ac88000b4543355d097ec"
   end
 
   depends_on "go" => :build
