623aba668efde894dcee511b9ceea5669fb294c0
Nicholas Dille <nicholas.dille@mailbox.org>
2021-07-14 09:44:38 +0200

    Updated checksums

--- Formula/loft-bin.rb
+++ Formula/loft-bin.rb
@@ -15,11 +15,11 @@ class LoftBin < Formula
   resource "binary" do
     on_linux do
       url "https://github.com/loft-sh/loft/releases/download/v1.13.1/loft-linux-amd64"
-      sha256 "c854821f39f007fd8992ba4927bec66e6a5382fa35fd9eab3b216fe8b5b29dff"
+      sha256 "b90436fab0ac2d006874a91311bf5b4341f3d666c3265bc2b7344894e3730ff3"
     end
     on_macos do
       url "https://github.com/loft-sh/loft/releases/download/v1.13.1/loft-darwin-amd64"
-      sha256 "83ecb91a9f07e79e0f83db5982f5f3f57a0f9109553408f65329545c3d9c14cd"
+      sha256 "c24297cccfe0bdf222165ea476b919c8f80c95e9fcace72c55401e9e11fd2554"
     end
   end
 
