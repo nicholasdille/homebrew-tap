830cddb9a95bb50928d9c0071434c398810e424c
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-07-20 14:25:04 +0000

    kubectl-build 0.1.3

--- Formula/kubectl-build.rb
+++ Formula/kubectl-build.rb
@@ -3,8 +3,8 @@ class KubectlBuild < Formula
   homepage "https://github.com/vmware-tanzu/buildkit-cli-for-kubectl"
 
   url "https://github.com/vmware-tanzu/buildkit-cli-for-kubectl.git",
-    tag:      "v0.1.2",
-    revision: "25b70efa9a1eb0aee1923f506d074c9b62eb068f"
+    tag:      "v0.1.3",
+    revision: "e76c756a93b2e6e02ea9ebc03a7bbcec7123c3a6"
   license "Apache-2.0"
   head "https://github.com/vmware-tanzu/buildkit-cli-for-kubectl.git"
 
