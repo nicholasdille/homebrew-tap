4ba9f8e248520b9fccf0f8576336405dc04c46e7
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-07-22 08:44:30 +0000

    reptyr: add 0.8.0 bottle.

--- Formula/reptyr.rb
+++ Formula/reptyr.rb
@@ -13,6 +13,11 @@ class Reptyr < Formula
     strategy :git
   end
 
+  bottle do
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/reptyr-0.8.0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f4da9a96d37dc5a45f15289c744897f4e6475794e87c2bed40527e4a0c314cc6"
+  end
+
   depends_on "gcc" => :build
   depends_on "make" => :build
   depends_on :linux
