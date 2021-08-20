d42d17adba30c933d209df1b213d110b37147529
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-08-20 11:03:59 +0000

    nerdctl-immortal: update 0.11.1 bottle.

--- Formula/nerdctl-immortal.rb
+++ Formula/nerdctl-immortal.rb
@@ -14,8 +14,8 @@ class NerdctlImmortal < Formula
   end
 
   bottle do
-    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/nerdctl-immortal-0.11.0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "dddb29c0aa0671a4024b240af3747742f0de90d07c50abcf064bad2c3c76211b"
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/nerdctl-immortal-0.11.1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f9f064f7f42e251317a806fd4d6f3a61034111e7dae5d011aa0f199b9a5244cf"
   end
 
   depends_on "immortal"
