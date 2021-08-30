fd256eef8cb525e7270f77fc11fe616a39e4e5f4
Nicholas Dille <nicholas.dille@mailbox.org>
2021-08-30 22:08:15 +0200

    Added kubescape

---
+++ Formula/kubescape.rb
@@ -0,0 +1,33 @@
+class Kubescape < Formula
+  desc "Test if Kubernetes is deployed securely"
+  homepage "https://github.com/armosec/kubescape"
+
+  url "https://github.com/armosec/kubescape.git",
+    tag:      "v1.0.52-beta",
+    revision: "798994850dd67f959cfd7b922f32d7d5dc6fbc48"
+  license "Apache-2.0"
+  head "https://github.com/armosec/kubescape.git"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go",
+      "mod",
+      "tidy"
+
+    system "go",
+      "build",
+      "-o",
+      bin/"kubescape",
+      "."
+  end
+
+  test do
+    system "whereis", "kubescape"
+  end
+end
