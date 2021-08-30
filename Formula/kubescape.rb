9faf77960065af1ba96466563bbe893a06c45d3d
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-08-30 21:07:43 +0000

    kubescape: add 1.0.52-beta bottle.

--- Formula/kubescape.rb
+++ Formula/kubescape.rb
@@ -14,6 +14,12 @@ class Kubescape < Formula
     strategy :git
   end
 
+  bottle do
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/kubescape-1.0.52-beta"
+    sha256 cellar: :any_skip_relocation, catalina:     "3c539bb0541458b61ae6dd9232785ea86886dc10e8a1c72f11b043283c3b676a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0493b8e68cf433bd4d051d81d7a8fc69f57e859e6fb47a7d34aaaa9f6433141c"
+  end
+
   depends_on "go" => :build
 
   def install
