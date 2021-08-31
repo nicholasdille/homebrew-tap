61f068b4115e2c7bf94961e038aca7724de27716
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-08-31 08:23:33 +0000

    kubescape: update 1.0.55 bottle.

--- Formula/kubescape.rb
+++ Formula/kubescape.rb
@@ -15,9 +15,9 @@ class Kubescape < Formula
   end
 
   bottle do
-    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/kubescape-1.0.52-beta"
-    sha256 cellar: :any_skip_relocation, catalina:     "3c539bb0541458b61ae6dd9232785ea86886dc10e8a1c72f11b043283c3b676a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "0493b8e68cf433bd4d051d81d7a8fc69f57e859e6fb47a7d34aaaa9f6433141c"
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/kubescape-1.0.55"
+    sha256 cellar: :any_skip_relocation, catalina:     "b62a0c0dc001ad815423a4f270aedc35f39fe32e9c02103f8c1fa9bd13a95776"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "04aa602f54584f6a29f9f43b3d583323c99bee2f57733d4b646a803cf6be29dc"
   end
 
   depends_on "go" => :build
