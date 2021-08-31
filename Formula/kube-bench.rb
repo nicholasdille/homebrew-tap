30489c612613dba29594b24c20000acf202039b1
Nicholas Dille <nicholas.dille@mailbox.org>
2021-08-31 15:29:47 +0200

    Added kube-bench

---
+++ Formula/kube-bench.rb
@@ -0,0 +1,37 @@
+class KubeBench < Formula
+  desc "Checks whether Kubernetes is deployed according to security best practices"
+  homepage "https://github.com/aquasecurity/kube-bench"
+
+  url "https://github.com/aquasecurity/kube-bench.git",
+    tag:      "v0.6.3",
+    revision: "75fe5d00482d36b7fecffcc1549d1a2184c740ba"
+  license "Apache-2.0"
+  head "https://github.com/aquasecurity/kube-bench.git"
+
+  livecheck do
+    url :stable
+    strategy :git
+  end
+
+  depends_on "go" => :build
+
+  def install
+    pkg = "github.com/aquasecurity/kube-bench/cmd"
+
+    ENV["CGO_ENABLED"] = "0"
+    system "go",
+      "build",
+      "-ldflags", "-s -w"\
+                  " -X #{pkg}.KubeBenchVersion=#{version}"\
+                  " -X #{pkg}.cfgDir=#{etc}/kube-bench",
+      "-o",
+      bin/"kube-bench",
+      "."
+
+    (etc/"kube-bench").install Dir["cfg/*"]
+  end
+
+  test do
+    system bin/"kube-bench", "version"
+  end
+end
