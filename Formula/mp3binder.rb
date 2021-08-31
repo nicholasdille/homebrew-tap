6980d7a7dd9203921b1e26b095c9f36da8beb740
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-08-31 18:52:31 +0000

    mp3binder: add 3.0.0 bottle.

--- Formula/mp3binder.rb
+++ Formula/mp3binder.rb
@@ -14,6 +14,12 @@ class Mp3binder < Formula
     strategy :git
   end
 
+  bottle do
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/mp3binder-3.0.0"
+    sha256 cellar: :any_skip_relocation, catalina:     "6725b69e64e5d9d57711480c5ea68a78fbd473a04aa353931e53c0bf8aa990f7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "952794b63dcf3b382da730fd5c1f8d13f0415529291439ff70c02d47b8d9e703"
+  end
+
   depends_on "go" => :build
   depends_on arch: :x86_64
 
