c7ff7c9c838097b616ef292b3a638fc4681c1157
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-09-07 21:03:29 +0000

    cni: update 1.0.1 bottle.

--- Formula/cni.rb
+++ Formula/cni.rb
@@ -15,8 +15,8 @@ class Cni < Formula
   end
 
   bottle do
-    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/cni-1.0.0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "d07b5e0a891231b1b069636630a75d47509fd39f6b1b277a4090daaaa1f77bec"
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/cni-1.0.1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9088348d8935c61dfa672269839d369ceb1517d366f44a6b2330f9d15ddde2c0"
   end
 
   depends_on "go" => :build
