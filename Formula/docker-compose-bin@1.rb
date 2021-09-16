c0b3e6abbc04aafea1fdbd546f08e1222109210b
Nicholas Dille <nicholas.dille@mailbox.org>
2021-09-16 09:40:03 +0200

    Pinned docker-compose to v1
    Closes #670.
    
    Signed-off-by: Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>

---
+++ Formula/docker-compose-bin@1.rb
@@ -0,0 +1,53 @@
+class DockerComposeBinAT1 < Formula
+  desc "Define and run multi-container applications with Docker"
+  homepage "https://docs.docker.com/compose/"
+
+  url "https://github.com/docker/compose.git",
+    tag:      "1.29.2",
+    revision: "5becea4ca9f68875334c92f191a13482bcd6e5cf"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^(1\..+)/i)
+  end
+
+  bottle :unneeded
+
+  keg_only :versioned_formula
+
+  resource "binary" do
+    on_linux do
+      url "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64"
+      sha256 "f3f10cf3dbb8107e9ba2ea5f23c1d2159ff7321d16f0a23051d68d8e2547b323"
+    end
+    on_macos do
+      url "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Darwin-x86_64"
+      sha256 "ddfaf03c3aae5277cd5f611f1b33d1966157ed9d3545a98307ae78d40cbd9684"
+    end
+  end
+
+  def install
+    resource("binary").stage do
+      os = "Linux"  if OS.linux?
+      os = "Darwin" if OS.mac?
+      bin.install "docker-compose-#{os}-#{Hardware::CPU.arch}" => "docker-compose"
+    end
+
+    bash_completion.install "contrib/completion/bash/docker-compose"
+    fish_completion.install "contrib/completion/fish/docker-compose.fish"
+    zsh_completion.install "contrib/completion/zsh/_docker-compose"
+  end
+
+  def caveats
+    <<~EOS
+      You are installing the Python-based docker-compose.
+      Please consider using the new Go-based CLI-integrated compose-cli:
+        brew install nicholasdille/tap/docker-compose-cli
+    EOS
+  end
+
+  test do
+    system bin/"docker-compose", "version"
+  end
+end
