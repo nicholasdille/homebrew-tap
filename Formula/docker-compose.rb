9911f01612158b422601b8c5f3f3a9acca0a0a9b
Nicholas Dille <nicholas.dille@mailbox.org>
2021-09-16 09:44:15 +0200

    Added docker-compose v2

---
+++ Formula/docker-compose.rb
@@ -0,0 +1,52 @@
+class DockerCompose < Formula
+  desc "Define and run multi-container applications with Docker"
+  homepage "https://docs.docker.com/compose/"
+
+  url "https://github.com/docker/compose.git",
+  tag:      "v2.0.0-rc.3",
+  revision: "1a73d1412d562e32effbe7b322f17e8d77ad7755"
+  license "Apache-2.0"
+  head "https://github.com/docker/compose.git",
+  branch: "v2"
+
+  livecheck do
+    url :stable
+    strategy :git
+  end
+
+  depends_on "go" => :build
+  depends_on "docker" => :optional
+
+  conflicts_with "docker-compose"
+
+  def install
+    pkg = "github.com/docker/compose/v2"
+    ENV["CGO_ENABLED"] = "0"
+
+    system "go",
+        "build",
+        "-trimpath",
+        "-tags", "e2e,kube",
+        "-ldflags", "-s -w"\
+                    " -X #{pkg}/internal.Version=v#{version}",
+        "-o", lib/"docker/cli-plugins/docker-compose",
+        "./cmd"
+
+    (bin/"com.docker.cli").write <<~EOS
+      #!/usr/bin/env bash
+      exec #{HOMEBREW_PREFIX}/bin/docker "$@"
+    EOS
+  end
+
+  def caveats
+    <<~EOS
+      You should create a symlink to enable the Docker CLI plugin:
+      mkdir -p $HOME/.docker/cli-plugins
+      ln -s #{lib}/docker/cli-plugins/docker-compose $HOME/.docker/cli-plugins
+    EOS
+  end
+
+  test do
+    system lib/"docker/cli-plugins/docker-compose", "compose", "version"
+  end
+end
