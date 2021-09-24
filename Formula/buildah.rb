aa1ba1201b24e11945bc98dcdb2f2ea3a54d974a
Nicholas Dille <nicholas.dille@mailbox.org>
2021-09-24 10:31:48 +0200

    Converted to dynamically linked binary

--- Formula/buildah.rb
+++ Formula/buildah.rb
@@ -19,54 +19,31 @@ class Buildah < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "65f3774b366e9993ab7afd0494c705b45534727bc719a54c24d118c30c13ae6c"
   end
 
+  option "with-btrfs", "Support BTRFS, requires libbtrfs-dev"
+  option "with-devmapper", "Support device mapper"
+
+  depends_on "go" => :build
   depends_on "go-md2man" => :build
   depends_on "make" => :build
-  depends_on "nicholasdille/tap/docker" => :build
+  depends_on "pkg-config" => :build
+  depends_on "gpgme"
+  depends_on "libassuan"
+  depends_on "libgpg-error"
   depends_on :linux
+  depends_on "libseccomp" => :recommended
 
   def install
-    # Build base from https://github.com/NixOS/docker
-    system "docker",
-      "build",
-      "--tag", "nix",
-      "github.com/NixOS/docker"
+    ENV.O0
 
-    # Create Dockerfile
-    (buildpath/"Dockerfile").write <<~EOS
-      FROM nix
-      RUN apk update \
-       && apk add \
-              bash \
-              make \
-              git \
-              go
-    EOS
+    buildtags = []
+    # buildtags << "apparmor" # libapparmor
+    buildtags << "seccomp" if build.with? "libseccomp"
+    buildtags << "exclude_graphdriver_btrfs" if build.without? "btrfs"
+    buildtags << "btrfs_noversion" if build.with? "btrfs"
+    buildtags << "libdm_no_deferred_remove" if build.without? "devmapper"
+    # buildtags << "libsubid" # shadow
 
-    # Build custom image
-    system "docker",
-      "build",
-      "--tag", "buildah",
-      "."
-
-    # Run build
-    system "docker",
-      "run",
-      "--interactive",
-      "--rm",
-      "--mount", "type=bind,src=#{buildpath},dst=/src",
-      "--workdir", "/src",
-      "buildah",
-      "make", "static"
-
-    system "docker",
-      "run",
-      "--interactive",
-      "--rm",
-      "--mount", "type=bind,src=#{buildpath},dst=/src",
-      "--workdir", "/src",
-      "alpine",
-      "chown", "-R", "#{Process.uid}:#{Process.gid}", "."
-
+    system "make", "bin/buildah", "TAGS=#{buildtags.join(",")}"
     bin.install "bin/buildah"
 
     bash_completion.install "contrib/completions/bash/buildah"
