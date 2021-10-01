8d91faa8c4cc228b7a3cc785b042cb10753af50d
Nicholas Dille <10155592+nicholasdille@users.noreply.github.com>
2021-10-01 07:23:55 +0000

    envcli: add 0.7.1 bottle.

--- Formula/envcli.rb
+++ Formula/envcli.rb
@@ -14,6 +14,12 @@ class Envcli < Formula
     strategy :git
   end
 
+  bottle do
+    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/envcli-0.7.1"
+    sha256 cellar: :any_skip_relocation, catalina:     "fa0419662a9d477df5e29ea2a639bf2a568ea0562974828873f5be94e6dfd46e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2a7e8c1179445791eca64456394398741c11f06843d74a6bf4a400fbc5942ccc"
+  end
+
   depends_on "go" => :build
 
   def install
