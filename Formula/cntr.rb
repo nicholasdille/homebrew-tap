0ce0431d75e13c669ba9976706403e7e97048f0d
Nicholas Dille <nicholas.dille@mailbox.org>
2021-10-01 08:26:22 +0200

    Added cntr (closes #268)

---
+++ Formula/cntr.rb
@@ -0,0 +1,28 @@
+class Cntr < Formula
+  desc "Container debugging tool based on FUSE"
+  homepage "https://github.com/Mic92/cntr"
+
+  url "https://github.com/Mic92/cntr.git",
+    tag:      "1.5.1",
+    revision: "fdc1463cdfea0d5007c15369703150767869af51"
+  license "MIT"
+  head "https://github.com/Mic92/cntr.git",
+    branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "make" => :build
+  depends_on "rust" => :build
+
+  def install
+    system "make", "all"
+    bin.install "target/release/cntr"
+  end
+
+  test do
+    system bin/"cntr", "--help"
+  end
+end
