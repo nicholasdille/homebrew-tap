8004ceed8ad7247191fe0aa7c8fa22cc7472318d
Nicholas Dille <nicholas.dille@mailbox.org>
2021-10-18 14:35:32 +0200

    Fixed type conversion

--- Formula/artifactory-cleanup.rb
+++ Formula/artifactory-cleanup.rb
@@ -31,6 +31,6 @@ class ArtifactoryCleanup < Formula
   end
 
   test do
-    assert_match version, shell_output("#{bin}/#{name} --version")
+    assert_match version.to_s, shell_output("#{bin}/#{name} --version")
   end
 end
