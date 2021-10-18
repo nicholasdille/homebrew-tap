4633836be70566515aa143a243f8bb38b00cd794
Nicholas Dille <nicholas.dille@mailbox.org>
2021-10-18 13:55:44 +0200

    Improve go build commands

--- Formula/artifactory-cleanup.rb
+++ Formula/artifactory-cleanup.rb
@@ -6,6 +6,7 @@ class ArtifactoryCleanup < Formula
     tag:      "v1.5.0",
     revision: "05479368a3050635e03478dd744c8e6ac3b39b70"
   license "MIT"
+  revision 1
   head "https://github.com/crazy-max/artifactory-cleanup.git",
     branch: "master"
 
@@ -24,16 +25,14 @@ class ArtifactoryCleanup < Formula
 
   def install
     ENV["CGO_ENABLED"] = "0"
-    system "go",
-      "build",
-      "-trimpath",
-      "-ldflags", "-s -w"\
-                  " -X main.Version=#{version}",
-      "-o", bin/"artifactory-cleanup",
+    system "go", "build",
+      *std_go_args(
+        ldflags: "-s -w -X main.Version=#{version}"
+      ),
       "./cmd/main.go"
   end
 
   test do
-    system bin/"artifactory-cleanup", "--version"
+    assert_match "#{version}", shell_output("#{bin}/#{name} --version")
   end
 end
