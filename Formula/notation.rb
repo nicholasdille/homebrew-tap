79ffd949839d98e7b72f522d3b4633053f3ea84c
Nicholas Dille <nicholas.dille@mailbox.org>
2021-10-25 20:42:03 +0200

    First release

--- Formula/notation.rb
+++ Formula/notation.rb
@@ -3,33 +3,26 @@ class Notation < Formula
   homepage "https://github.com/notaryproject/notation"
 
   url "https://github.com/notaryproject/notation.git",
-    tag: "main"
-  version "0.0.0"
+    tag:      "v0.7.0-alpha.1",
+    revision: "2f51db34942fbc17a1992d88c0b282e9e3e85a1b"
   license "Apache-2.0"
   head "https://github.com/notaryproject/notation.git",
     branch: "main"
 
   livecheck do
-    skip "No version information available"
-  end
-
-  bottle do
-    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/notation-0.0.0"
-    sha256 cellar: :any_skip_relocation, big_sur:      "1cf162ed99f375a8e129c368535053aad0215281568beaf67403f07915bdf2c5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "5250ad9fe5293455a8eb278d92c34e1d1d514ada6da07365ac0e6681c42a7b7d"
+    url :stable
+    strategy :git
   end
 
   depends_on "go" => :build
 
   def install
-    commit = Utils.git_short_head
-
     ENV["CGO_ENABLED"] = "0"
 
     system "go",
       "build",
       "-ldflags", "-s -w"\
-                  " -X $(MODULE)/internal/version.BuildMetadata=#{commit}.unreleased",
+                  " -X $(MODULE)/internal/version.BuildMetadata=v#{version}",
       "-o",
       bin/"notation",
       "./cmd/notation"
@@ -37,7 +30,7 @@ class Notation < Formula
     system "go",
       "build",
       "-ldflags", "-s -w"\
-                  " -X $(MODULE)/internal/version.BuildMetadata=#{commit}.unreleased",
+                  " -X $(MODULE)/internal/version.BuildMetadata=v#{version}",
       "-o",
       bin/"docker-generate",
       "./cmd/docker-generate"
@@ -45,7 +38,7 @@ class Notation < Formula
     system "go",
       "build",
       "-ldflags", "-s -w"\
-                  " -X $(MODULE)/internal/version.BuildMetadata=#{commit}.unreleased",
+                  " -X $(MODULE)/internal/version.BuildMetadata=v#{version}",
       "-o",
       bin/"docker-notation",
       "./cmd/docker-notation"
