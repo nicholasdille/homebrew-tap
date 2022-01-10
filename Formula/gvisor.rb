ccb43d2d37a015ee2d227be8a09cce4df16a3e8a
Nicholas Dille <nicholas.dille@mailbox.org>
2022-01-10 16:19:21 +0100

    Fixed version

--- Formula/gvisor.rb
+++ Formula/gvisor.rb
@@ -5,7 +5,7 @@ class Gvisor < Formula
   url "https://github.com/google/gvisor.git",
     tag:      "release-20220103.0",
     revision: "b488df0a2f15ba21603610bdb13d8656b856218f"
-  version "20220103.0"
+  version "20220103"
   license "Apache-2.0"
   head "https://github.com/google/gvisor.git",
     branch: "master"
@@ -14,12 +14,12 @@ class Gvisor < Formula
   depends_on "nicholasdille/tap/cni"
 
   resource "runsc" do
-    url "https://storage.googleapis.com/gvisor/releases/release/20220103.0/x86_64/runsc"
+    url "https://storage.googleapis.com/gvisor/releases/release/20220103/x86_64/runsc"
     sha256 "6be7e228dbb1a9e8bb1c443183de9f2910ceb3aeb67c93d1d899358b117d9b5e"
   end
 
   resource "containerd-shim-runsc-v1" do
-    url "https://storage.googleapis.com/gvisor/releases/release/20220103.0/x86_64/containerd-shim-runsc-v1"
+    url "https://storage.googleapis.com/gvisor/releases/release/20220103/x86_64/containerd-shim-runsc-v1"
     sha256 "797e10587af1fdca4dc3d00577bd9a90a91a5e88c7d1e9b3eadad7335ab032c8"
   end
 
