dd6a3e9f632dc1961a5f50cc4a7354def6a6b07a
Nicholas Dille <nicholas.dille@mailbox.org>
2022-01-15 23:05:07 +0100

    Updated to build using go-task

--- Formula/mp3binder.rb
+++ Formula/mp3binder.rb
@@ -5,7 +5,7 @@ class Mp3binder < Formula
   url "https://github.com/crra/mp3binder.git",
     tag:      "4.0.0",
     revision: "f73a2b631c9bc08efeab0c5ae35bce1b28a89656"
-  license "Unlicense"
+  license "MIT"
   head "https://github.com/crra/mp3binder.git",
     branch: "master"
 
@@ -14,24 +14,13 @@ class Mp3binder < Formula
     strategy :git
   end
 
-  bottle do
-    root_url "https://github.com/nicholasdille/homebrew-tap/releases/download/mp3binder-4.0.0_1"
-    sha256 cellar: :any_skip_relocation, catalina:     "7490828fbbe5933b0f60613b2f92e14e832956b23a52406c4c5ddab8729222f0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "0de5687bec86b568e36f49efe22e1ccb5f55023ec9eb4767e57737cf9a1dd901"
-  end
-
+  depends_on "go-task/tap/go-task" => :build
   depends_on "go" => :build
   depends_on arch: :x86_64
 
   def install
-    ENV["CGO_ENABLED"] = "0"
-    system "go",
-      "run",
-      "cmd/build/build.go", "-p"
-
-    os = "linux" if OS.linux?
-    os = "darwin" if OS.mac?
-    bin.install "dist/release/#{os}/mp3binder"
+    system "task", "build"
+    bin.install "dist/linux_amd64/mp3binder"
   end
 
   test do
